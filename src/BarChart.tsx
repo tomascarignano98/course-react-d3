d32897c1e3724315ab722860953a9ad8eb208f01
tomascarignano <tomas@festinafinance.com>
2023-09-05 17:24:02 +0200

    declare chart scales

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -1,3 +1,5 @@
+import * as d3 from "d3";
+
 type BarChartProps = {
   width: number;
   height: number;
@@ -5,7 +7,17 @@ type BarChartProps = {
 };
 
 const BarChart = ({ width, height, data }: BarChartProps) => {
-  console.log(data);
+  const margin = 10;
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.sunshine) ?? 1])
+    .range([0, width - margin]);
+
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.city))
+    .range([height - 2 * margin, 0]);
 
   return <svg viewBox={`0 0 ${width} ${height}`}></svg>;
 };
